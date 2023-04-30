fea1fecc4ffe721148d11f665792245fcf19c250
aotra <89144623+aotra@users.noreply.github.com>
2023-04-30 23:51:04 +0100

    Uploaded Files

---
+++ README.md
@@ -0,0 +1,45 @@
+# Reddit Imgur Saver
+This basic script uses the Reddit API to go through your **saved posts** and download any images/videos that are hosted on imgur
+* **!! This script currently is unable to download imgur galleries/albums, but provides links to all of these at the end of the program for manual backup !!**
+
+## Setup ##
+First go to https://www.reddit.com/prefs/apps and create an app
+![image](https://user-images.githubusercontent.com/89144623/235378724-e4e65518-2d15-48f9-a6cb-52803209e904.png)
+
+**!! Make sure to select script !!** 
+
+Enter in anything for all other fields
+
+Once made, write down the client ID and client secret
+
+![image](https://user-images.githubusercontent.com/89144623/235378869-ec2e5f8f-bd99-46ea-b0e5-9b89bef1aaed.png)
+
+Once completed you can move onto preparing and running the script
+
+## Installation and prep ##
+
+Clone or download the repo
+
+Open a terminal in the folder and install the requirements
+
+`pip install -r requirements.txt`
+
+To complete the preparation, open credentials.json and input the client id and client secret from earlier, as well as your reddit username and password. 
+
+**!! The account you wish to save from must be the same as the one you created the bot on earlier !!**
+
+![image](https://user-images.githubusercontent.com/89144623/235379095-e19c0d29-a279-4c9b-8135-95fc2fb3b3ae.png)
+
+## Running the script ##
+
+Finally, run `python main.py` to start the saving process
+
+The script will create a unique folder in the directory where it will save all images/videos 
+
+The script will additionally create 2 text files containing all imgur links from your reddit saved as well as all of your saved posts + titles in case they are of use to you
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,129 @@
+import praw
+import re
+import os
+import requests
+import urllib.request
+import json
+
+# Clean up
+path = os.path.join("./", "imgur_links.txt")
+if os.path.exists(path):
+    os.remove(path)
+path = os.path.join("./", "saved_posts.txt")
+if os.path.exists(path):
+    os.remove(path)
+
+# Define the output folder name prefix
+folder_prefix = 'imgur_downloads'
+
+# Find the highest numbered folder
+max_num = -1
+for folder_name in os.listdir('.'):
+    if folder_name.startswith(folder_prefix):
+        folder_num = re.findall(r'\d+', folder_name)
+        if folder_num:
+            max_num = max(max_num, int(folder_num[0]))
+
+# Generate the new folder name
+new_folder_name = f"{folder_prefix}{max_num+1}"
+
+# Create the new folder
+os.makedirs(new_folder_name)
+
+with open('credentials.json') as file_object:
+    credentials = json.load(file_object)
+
+
+# Create a Reddit instance
+reddit = praw.Reddit(client_id=credentials["CLIENT_ID"],
+                     client_secret=credentials["CLIENT_SECRET"],
+                     username=credentials["REDDIT_USER"],
+                     password=credentials["REDDIT_PASS"],
+                     user_agent='Saved Imgur Scraper')
+
+# get the first saved post of a user
+saved_posts = reddit.user.me().saved(limit=None)
+
+with open('saved_posts.txt', 'w') as f:
+    # iterate over the saved posts and print the title and URL of each post
+    for post in saved_posts:
+        try:
+            print(post.title)
+            print(post.url)
+            # write post title and URL to file
+            f.write(post.title + '\n')
+            f.write(post.url + '\n')
+
+            # write links within post to file
+            post_content = post.selftext
+            urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', post_content)
+            if urls:
+                for url in urls:
+                    print(url)
+                    f.write(f'{url}\n')
+
+        except Exception:
+            pass
+
+# print a message indicating the file has been written
+print('Saved posts written to saved_posts.txt')
+
+# open the file containing the saved posts
+with open('saved_posts.txt', 'r') as f:
+    # read the file contents into a variable
+    file_contents = f.read()
+
+# use regular expressions to match URLs in the file contents that start with i.imgur.com
+imgur_urls = re.findall(r'https?://(?:i\.)?imgur\.com/\S+', file_contents)
+
+# count the number of imgur links
+num_imgur_links = len(imgur_urls)
+
+# print the number of imgur links
+print(f'Total number of imgur links: {num_imgur_links}')
+
+with open('imgur_links.txt', 'w') as f:
+    # iterate over the saved posts and print the title and URL of the first post
+    for url in imgur_urls:
+        if "account" not in url:
+            try:
+                f.write(url + '\n')
+            except Exception:
+                pass
+
+count = 0
+
+# Open the file containing imgur links
+with open('imgur_links.txt', 'r') as f:
+    manuals = []
+    # iterate over each link
+    for link in f:
+        # Remove any leading or trailing whitespace
+        link = link.strip()
+        
+        # Check if the link is a gallery
+        if "/a/" in link or "/gallery/" in link:
+            manuals.append(link + "\n")
+            
+        # If the link is not a gallery, assume it's an image or video
+        else:
+            # Build the file name
+            file_name = os.path.join(new_folder_name, os.path.basename(link))
+            
+            # Download the file
+            try:
+                urllib.request.urlretrieve(link, file_name)
+                
+                # Print a message indicating the file has been downloaded
+                print(f"{file_name} downloaded successfully")
+                count = count + 1
+            except Exception as e:
+                print(f"Error downloading {link}: {e}")
+
+print(f"{count} files were successfully downloaded.")
+if len(manuals) > 0:
+    print("-------------------------------------------------------------------------------------------------------------------------")
+    print("\n !! Since this program does not support downloading imgur galleries/albums, the following links need to be backed up manually !! \n")
+    print("-------------------------------------------------------------------------------------------------------------------------")
+    for link in manuals:
+        print(link)
